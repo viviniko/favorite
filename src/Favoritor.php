87c416e7a80cd20c227aa92dda06e47a2ca2f595
viviniko <viviniko.shop@gmail.com>
2019-08-10 14:18:54 +0800

    :trollface:

---
+++ src/Favoritor.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Viviniko\Favorite;
+
+use Illuminate\Support\Facades\Config;
+
+trait Favoritor
+{
+    public function favorites()
+    {
+        return $this->hasMany(Config::get('favorite.favorite'), 'user_id');
+    }
+
+    public function favorite($favoritable)
+    {
+        $favoritable->favoriteBy($this->id);
+        return $this;
+    }
+
+    public function unfavorite($favoritable)
+    {
+        $favoritable->unfavoriteBy($this->id);
+        return $this;
+    }
+}
